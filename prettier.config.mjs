0d15ceaf15751c2e74829555b0cf2df8874c57ae
perako2023 <135764815+perako2023@users.noreply.github.com>
2023-10-31 20:42:02 -0500

    chore: Format all with prettier

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+	plugins: ['prettier-plugin-tailwindcss'],
+	singleQuote: true,
+	semi: false,
+	tabWidth: 4,
+	printWidth: 100,
+	useTabs: true,
+}
 
-export default config;
+export default config
